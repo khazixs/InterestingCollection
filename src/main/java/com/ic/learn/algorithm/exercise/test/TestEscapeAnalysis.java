8a846dd24eecabf63e6126e40421c0a2961cc425
IceCoLaG <heiyubaidejianying@gmail.com>
2020-07-27 19:54:12 +0800

    估算对象大小

---
+++ src/main/java/com/ic/learn/algorithm/exercise/test/TestEscapeAnalysis.java
@@ -0,0 +1,9 @@
+package com.ic.learn.algorithm.exercise.test;
+
+public class TestEscapeAnalysis {
+    public static void main(String[] args) {
+        while (true){
+            Integer i = new Integer(12345);
+        }
+    }
+}
