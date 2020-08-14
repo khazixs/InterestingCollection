48c37138bac50cbb97cd0b5eaf231db9334a93aa
IceCoLaG <heiyubaidejianying@gmail.com>
2020-08-14 19:51:50 +0800

    提交

---
+++ src/main/java/com/ic/learn/algorithm/exercise/Divide.java
@@ -0,0 +1,37 @@
+package com.ic.learn.algorithm.exercise;
+
+import org.junit.Test;
+import sun.nio.ch.ThreadPool;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class Divide {
+    @Test
+    public void test(){
+        System.out.println(divide(10,3));
+    }
+    public int divide(int dividend, int divisor) {
+        boolean sign = (dividend > 0) ^ (divisor > 0);
+        int result = 0;
+        if(dividend>0) {
+            dividend = -dividend;
+        }
+        if(divisor>0) divisor = -divisor;
+        while(dividend <= divisor) {
+            int temp_result = -1;
+            int temp_divisor = divisor;
+            while(dividend <= (temp_divisor << 1)) {
+                if(temp_divisor <= (Integer.MIN_VALUE >> 1))break;
+                temp_result = temp_result << 1;
+                temp_divisor = temp_divisor << 1;
+            }
+            dividend = dividend - temp_divisor;
+            result += temp_result;
+        }
+        if(!sign) {
+            if(result <= Integer.MIN_VALUE) return Integer.MAX_VALUE;
+            result = - result;
+        }
+        return result;
+    }
+}
